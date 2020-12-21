effbdce2706272aa1fda499ae7e0eb5b4199681e
John Sarracino <jsarraci@cs.ucsd.edu>
2020-12-21 15:04:57 -0800

    add all the things

--- inferui/ast.py
+++ inferui/ast.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from ..common.run import View
 
 from typing import List
 
@@ -19,8 +20,16 @@ class Rendering:
   resolution: Resolution
   boxes: List[Box]
 
+  def to_view(self) -> View:
+    kids: List[View] = []
+    for i,box in enumerate(self.boxes):
+      child = View(name="child_%d" % i, rect=list(map(float, [box.left, box.top, box.right, box.bottom])))
+      kids.append(child)
+    return View(name="root", rect=[0.0, 0.0, float(self.resolution.width), float(self.resolution.height)])
+
 @dataclass
 class Layout:
   id: int
   renderings: List[Rendering]
+
 
